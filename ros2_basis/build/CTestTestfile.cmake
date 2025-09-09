6679b131f6d9a292277b7d07b03a96e28ee12f9d
Wang_mj <wxm43@foxmail.com>
2025-09-09 22:35:10 +0800

    ros2 节点/功能包

---
+++ ros2_basis/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/tian/ROS_ws/ros2_basis
+# Build directory: /home/tian/ROS_ws/ros2_basis/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
--- ros2_basis/first_node.cpp
+++ ros2_basis/first_node.cpp
@@ -1,4 +1,8 @@
 #include "rclcpp/rclcpp.hpp"
-int main(int argc,char **argv) {
-    
+int main(int argc,char **argv) {//必须写int argc,char **argv，节点需要解析这些参数
+    rclcpp::init(argc, argv);
+    // make_shared<T>(args..)作用是在堆上创建一个对象，并返回一个 std::shared_ptr<T> 智能指针
+    //std::shared_ptr<T> 智能指针会自动管理内存 当引用计数归零时自动释放
+    rclcpp::spin(std::make_shared<rclcpp::Node>("first_node"));
+    return 0;
 }
